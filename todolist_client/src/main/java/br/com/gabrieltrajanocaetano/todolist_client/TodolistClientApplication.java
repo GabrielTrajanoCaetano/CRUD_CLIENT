8bd6a2dab38515ed5d97a7feac8e3ea0b587c4cb
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-07-28 10:05:46 -0300

    finish add dependencies

---
+++ todolist_client/src/main/java/br/com/gabrieltrajanocaetano/todolist_client/TodolistClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieltrajanocaetano.todolist_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistClientApplication.class, args);
+	}
+
+}
---
+++ todolist_client/src/test/java/br/com/gabrieltrajanocaetano/todolist_client/TodolistClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieltrajanocaetano.todolist_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
