f65b6702d39a45b01cbbceb2aefb18b3a9503f5e
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-07-28 10:14:06 -0300

    updating project

--- src/main/java/br/com/gabrieltrajanocaetano/desafio_todolist/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.gabrieltrajanocaetano.desafio_todolist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.gabrieltrajanocaetano.desafio_todolist.entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-    
-}
